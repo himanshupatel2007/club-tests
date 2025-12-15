e867326dc73553a77726771fb6c0c7b1e41f7c47
himanshupatel2007 <himanshupatel.942007@gmail.com>
2025-12-15 19:09:41 +0530

    done

---
+++ test2/index.js
@@ -0,0 +1,32 @@
+
+const listbox = document.querySelector(".list-items")
+const heading = document.querySelector("h2")
+const cleaerButton = document.querySelector("button")
+const items = document.querySelectorAll("li")
+
+
+let selected = 0
+
+items.forEach((e) => {
+    e.addEventListener("click", () => {
+
+        if (e.classList.contains("clicked")) {
+            e.classList.remove("clicked")
+            selected--
+        }
+        else {
+            e.classList.add("clicked")
+            selected++
+        }
+        heading.textContent = `Selected Items : ${selected}`
+    })
+})
+
+
+
+cleaerButton.addEventListener("click", () => [
+    items.forEach((e) => {
+        e.classList.remove("clicked")
+    })
+])
+
